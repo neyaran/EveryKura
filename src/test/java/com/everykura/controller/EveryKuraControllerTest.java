c214c2dc41b329a08466b409b8703ba4ae97a268
Narayan Neopane <narayanneopane@hotmail.com>
2017-03-18 18:21:18 -0400

    fix junit test method

--- src/test/java/com/everykura/controller/EveryKuraControllerTest.java
+++ src/test/java/com/everykura/controller/EveryKuraControllerTest.java
@@ -1,14 +1,12 @@
 package com.everykura.controller;
 
-import static org.junit.Assert.*;
-
 import org.junit.Test;
 
 public class EveryKuraControllerTest {
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		
 	}
 
 }
