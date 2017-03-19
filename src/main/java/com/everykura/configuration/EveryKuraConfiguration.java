dbf77289e2e27ff94c5bbe5ca9245a629d0537de
Narayan Neopane <narayanneopane@hotmail.com>
2017-03-18 23:28:12 -0400

    add resource handler to include static files

--- src/main/java/com/everykura/configuration/EveryKuraConfiguration.java
+++ src/main/java/com/everykura/configuration/EveryKuraConfiguration.java
@@ -1,26 +1,29 @@
 package com.everykura.configuration;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = "com.everykura")
-public class EveryKuraConfiguration {
+public class EveryKuraConfiguration extends WebMvcConfigurerAdapter {
 
-	@Bean(name="EveryKura")
-	public ViewResolver viewResolver() {
+	@Override
+	public void configureViewResolvers(ViewResolverRegistry registry) {
 		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
 		viewResolver.setViewClass(JstlView.class);
 		viewResolver.setPrefix("/WEB-INF/views/");
 		viewResolver.setSuffix(".jsp");
-		
-		return viewResolver;
+		registry.viewResolver(viewResolver);
 	}
-
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry){
+		registry.addResourceHandler("/static/**").addResourceLocations("/static/");
+	}
 }
